42fe3183caebeaa927c1793a832b4c2a7f5dfd95
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-13 17:07:03 +0300

    config settings (indent value and debug mode)

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :elibuf,
+    indent_value: "\t",
+    debug_mode: true,
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
